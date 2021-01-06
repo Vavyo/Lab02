73f40a6d95ae4f43a851736e6c21eeb24db272ec
Nathan Hall <vavyo2121@gmail.com>
2021-01-05 22:44:12 -0600

    quiz bug fixes and clean up

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 "use strict";
 
-console.log('app.js connected to index');
+//console.log('app.js connected to index');
 
 var userInput;
-console.log(userInput);
 
 function validate(input, compare){
   var result = false;
@@ -24,29 +23,29 @@ function validate(input, compare){
 var answers = [,,,,];
 
 function startQuiz(){
-  console.log('click');
+  //console.log('click');
 
   //give quiz
   userInput = prompt("Was I born in Des Moines?");
   answers[0] = validate(userInput, false);
-  console.log(answers);
+  //console.log(answers);
   userInput = prompt("Did I attend public school?");
   answers[1] = validate(userInput, true);
-  console.log(answers);
+  //console.log(answers);
   userInput = prompt("Do I work in the food industry?");
   answers[2] = validate(userInput, false);
-  console.log(answers);
+  //console.log(answers);
   userInput = prompt("Did I get a diploma?");
   answers[3] = validate(userInput, true);
-  console.log(answers);
+  //console.log(answers);
   userInput = prompt("Am I interested in Computers?");
   answers[4] = validate(userInput, true);
-  console.log(answers);
+  //console.log(answers);
 
   // return result
   var i;
   var result = true;
-  for(i = 0; i<answers.length(); i++){
+  for(i = 0; i<answers.length; i++){
     if(!answers[i]){
       result = false;
       break;
