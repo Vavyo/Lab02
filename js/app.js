ed27876f83d0f7532d26b0bb9065875fc5ad59aa
Nathan Hall <vavyo2121@gmail.com>
2021-01-14 17:21:16 -0600

    added the lab 3 requirements and made my own version of the number question.

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 //console.log('app.js connected to index');
 
 //get user
-// var userName = prompt("Who are you?");
-// alert("Welcome to my page, " + userName + "!");
+var userName = prompt("Who are you?");
+alert("Welcome to my page, " + userName + "!");
 
 //declare variables
 var userInput;
-var answers = [,,,,];
 var score = 0;
+var attemptsRemaining;
+
 
 //Checks answers to a given boolean key
 function validate(input, compare){
@@ -24,7 +25,7 @@ function validate(input, compare){
       result = true
     }
   }
-  return(result);
+  if (result === true){score++;}
 }
 
 //function called when the Attempt Quiz button element is clicked
@@ -33,90 +34,52 @@ function startQuiz(){
 
   //give quiz
   userInput = prompt("Was I born in Des Moines?");
-  answers[0] = validate(userInput, false);
+  validate(userInput, false);
   //console.log(answers);
   userInput = prompt("Did I attend public school?");
-  answers[1] = validate(userInput, true);
+  validate(userInput, true);
   //console.log(answers);
   userInput = prompt("Do I work in the food industry?");
-  answers[2] = validate(userInput, false);
+  validate(userInput, false);
   //console.log(answers);
   userInput = prompt("Did I get a diploma?");
-  answers[3] = validate(userInput, true);
+  validate(userInput, true);
   //console.log(answers);
   userInput = prompt("Am I interested in Computers?");
-  answers[4] = validate(userInput, true);
+  validate(userInput, true);
   //console.log(answers);
+}
 
-  // return result
-  var i;
-  var result = true;
-  for(i = 0; i<answers.length; i++){
-    if(!answers[i]){
-      result = false;
+
+function numberQuestion(){
+  for(attemptsRemaining=4;attemptsRemaining>0;attemptsRemaining--){
+    do{
+      userInput = parseInt(prompt("Guess a number. Attempts remaining: " + attemptsRemaining));
+    }while(isNaN(userInput))
+    if(userInput === 3){
+      alert('You nailed it! The answer was 3!');
+      score ++;
       break;
+    }else if(userInput > 3){
+      alert('Too high!');
+    }else if(userInput < 3){
+      alert('Too low!');
+    }
+    if(i === 1){
+      alert('Sorry the answer was 3.');
     }
   }
-  if(result){
-    alert("Congrats, " + userName + "! You passed!");
-  }else{
-    alert("Sorry, " + userName + ". You failed.");
-  }
 }
-
-
-function RNGQuestion(){
-  var number = Math.floor(Math.random() * 10) + 1;   // Found the random generator on W3 schools (https://www.w3schools.com/js/js_random.asp)
-    // console.log('The random number is: ', number);
-    var guessCounter = 0;
-    var loopCounter = 4;
-    var right = false
-    // create a while loop with if statements to check for correct answers.
-    while(guessCounter < 4){
-        var question6 = parseInt(prompt('Guess a number between 1 and 10. You have ' + loopCounter + ' guesses remaining.'));
-        
-        while(question6){
-
-          alert('try again');
-          question6 = parseInt(prompt('Guess a number between 1 and 10. You have ' + loopCounter + ' guesses remaining.'));
-          console.log(question6)
-        }
-        console.log(question6);
-
-        loopCounter -= 1;
-        // console.log(question6)
-        if(question6 !== number && question6 > number){
-            alert("You guessed too high! Guess again!");
-            guessCounter += 1;
-        }else if(question6 !== number && question6 < number){
-            alert("You guessed too low! Guess again!");
-            guessCounter += 1;
-        }
-        else{
-            alert('You got lucky! Good job.');
-            score += 1;
-            guessCounter = 4;
-            right = true
-        }
-        // console.log(guessCounter);
-    } 
-    if(right === false){
-        alert('You did not guess the answer. The right answer was: ' + number);
-    }
-}
-var score = 0;
 
 function travelQuestions(){
   var locations = ['canada','washington dc','wyoming'];
-  var attemptsRemaining = 6;
-  var guess;
-  var i;
+  attemptsRemaining = 6;
   while(attemptsRemaining > 0){
     console.log('attempts remaining: ' + attemptsRemaining);
     attemptsRemaining--;
-    guess = prompt('Where have I traveled out of state? (answer could be a country or a state!)');
-    for (i = 0; i<locations.length; i++){
-      if(guess.toLowerCase()=== locations[i]){
+    userInput = prompt('Where have I traveled out of state? (answer could be a country or a state!)').toLowerCase();
+    for (var i = 0; i<locations.length; i++){
+      if(userInput === locations[i]){
         alert('Congrats! You nailed it!');
         score++
         attemptsRemaining = 0;
@@ -124,6 +87,7 @@ function travelQuestions(){
       }
     }
   }
+  alert('The possible answers were: ' + locations[0] + ', ' + locations[1] + ', ' + locations[2])
 
-alert('Your final score is: ' +score);
+  alert('Your final score is: ' +score);
 }
